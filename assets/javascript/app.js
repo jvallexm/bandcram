f128b40ebc6bb0f100c9726a690449e19d93d0c1
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-01-23 17:43:39 -0500

    Added youtube function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ const yt_api_key = `AIzaSyA07NHdSXAhv8cLIyND8qsb4Uvwt0-DVgE`; //YouTube API Key
 
 // Searches YouTube for a single video q and returns a Youtube video iframe
 
-function getYouTubeVideo(q){
+function getYouTubeVideo(q,div){
 
     $.get(`https://www.googleapis.com/youtube/v3/search`, 
     	  {		
@@ -22,12 +22,20 @@ function getYouTubeVideo(q){
 
           },function(data){
 
-              console.log(data);
+          	  console.log(data);
+              $("<iframe>").attr("src", "https://www.youtube.com/embed/" + data.items[0].id.videoId)
+             			   .attr("frameborder","0").appendTo("#" + div);
 
           });
+    
 }
 
-getYouTubeVideo("village people");
+$(document).ready(function(){
+
+	getYouTubeVideo("village people","video");
+
+});
+
 
 // Renders all three things at once when data has been returned
 
