37d34c8e1ecbfaf63d01c1b0075caf08cb084804
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-01-22 19:04:18 -0500

    Added one user story

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 const recipeUrl="https://api.edamam.com/search?app_id=e5161b01&app_key=5f83606b6f6bf1a7278cd8ff994302aa&q=";
 
+// Gets recipes as an array from the Edama API
+
 function getRecipes(q){
 
 	$.ajax({
