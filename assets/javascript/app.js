cdeb7dc7dd4830cbf0f39fa09f3d4ede14273caf
David Coulombe <David.Coulombe7@gmail.com>
2018-01-27 11:09:28 -0500

    Time function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,19 @@ const eventful_api_key = `G6bFxWDSpqCDTwjr`;                           // Eventf
 const yt_api_key = `AIzaSyA07NHdSXAhv8cLIyND8qsb4Uvwt0-DVgE`;          // YouTube API Key
 const google_places_key = `AIzaSyDvotQLuJNpv-ba_5nzrBnkAzZP6DutQ7E`;   // Google Places API Key
 
-function getEvents(q,where,date,results,near) // Gets events from the Eventful API
+function getEvents(q, where, date, results, near) // Gets events from the Eventful API
 {
 
     // API Query parameters
     var oArgs = {
-      
-      app_key: eventful_api_key,
-      q: q, 
-      where: where, 
-      date: date,
-      page_size: results,
-      sort_order: "popularity"
-      
+
+        app_key: eventful_api_key,
+        q: q,
+        where: where,
+        date: date,
+        page_size: results,
+        sort_order: "popularity"
+
     };
 
     EVDB.API.call("/events/search", oArgs, function (oData) {
@@ -25,15 +25,13 @@ function getEvents(q,where,date,results,near) // Gets events from the Eventful A
         for (let i = 0; i < eventsArray.length; ++i) {
 
             let event = eventsArray[i]; //The current event
-            
+
             let eventTime = event.start_time;
-            console.log(eventTime);
-            let eventTimeFormat = moment(eventTime).format("dddd, MMMM Do YYYY, h:mm a");
-            console.log(eventTimeFormat);
+            let displayTime = timeFormat(eventTime);
             // console.log(moment())
             // Let's get the time and date, the format is YYYY-MM-DD HH:MM:SS
 
-            renderResult(i,event.title,`${eventTimeFormat} ${event.venue_address} ${event.city_name}, ${event.region_abbr} ${event.postal_code}` ); // Creates a new panel for each of the returned events
+            renderResult(i, event.title, `${displayTime} ${event.venue_address} ${event.city_name}, ${event.region_abbr} ${event.postal_code}`); // Creates a new panel for each of the returned events
 
             // console.log(event.title);        // Event Title
             console.log(event);              // Event Object
@@ -43,24 +41,24 @@ function getEvents(q,where,date,results,near) // Gets events from the Eventful A
 
             // Sets the search variable based on the event data returned
 
-            if(event.performers === null)
-               search = event.title;
+            if (event.performers === null)
+                search = event.title;
 
-            else if(typeof(event.performers) === "array")
-               search = event.performers[0].name;
+            else if (typeof (event.performers) === "array")
+                search = event.performers[0].name;
 
-            else if(typeof(event.performers) === "object")
-               search = event.performers.performer.name;
+            else if (typeof (event.performers) === "object")
+                search = event.performers.performer.name;
 
             else
-               search = event.title;
+                search = event.title;
 
             // Renders a YouTube video based on the search and index i 
-            getYouTubeVideo(search,i);
+            getYouTubeVideo(search, i);
 
             // Renders a Google Map based on the search and index i, the latitude and longitude of the event location
             // The near parameter and 1000 meters 
-            getGoogleMap(i,event.latitude,event.longitude,"map-" + i, near, 1000);  
+            getGoogleMap(i, event.latitude, event.longitude, "map-" + i, near, 1000);
 
 
         }
@@ -69,37 +67,39 @@ function getEvents(q,where,date,results,near) // Gets events from the Eventful A
 
 }
 
-function getEventById(id,i) // Gets events from the Eventful API
+function getEventById(id, i) // Gets events from the Eventful API
 {
 
     // API Query parameters
     var oArgs = {
-      
-      app_key: eventful_api_key,
-      id: id, 
-      
+
+        app_key: eventful_api_key,
+        id: id,
+
     };
 
     EVDB.API.call("/events/get", oArgs, function (event) {
 
         // Events array from api call 
-  
+
         console.log(event);
-        renderResult(i,event.title,`${event.address} ${event.city}, ${event.region_abbr} ${event.postal_code}` );
+        let eventTime = event.start_time;
+        let displayTime = timeFormat(eventTime);
+        renderResult(i, event.title, `${displayTime} ${event.address} ${event.city}, ${event.region_abbr} ${event.postal_code}`);
         let search;
-        if(event.performers === null)
-               search = event.title;
+        if (event.performers === null)
+            search = event.title;
 
-        else if(typeof(event.performers) === "array")
-               search = event.performers[0].name;
+        else if (typeof (event.performers) === "array")
+            search = event.performers[0].name;
 
-        else if(typeof(event.performers) === "object")
-               search = event.performers.performer.name;
+        else if (typeof (event.performers) === "object")
+            search = event.performers.performer.name;
 
         else
-               search = event.title;
-        getYouTubeVideo(search,i);
-        getGoogleMap(i,event.latitude,event.longitude,"map-" + i, "Parking", 1000);  
+            search = event.title;
+        getYouTubeVideo(search, i);
+        getGoogleMap(i, event.latitude, event.longitude, "map-" + i, "Parking", 1000);
 
     });
 
@@ -108,7 +108,7 @@ function getEventById(id,i) // Gets events from the Eventful API
 
 // Renders a Google Map
 
-function getGoogleMap(i,lat, lon, div, near, radius){
+function getGoogleMap(i, lat, lon, div, near, radius) {
 
     // Renders the card title for the map
     $("#map-title-" + i).text(`${near} within ${radius}M`);
@@ -142,7 +142,7 @@ function getGoogleMap(i,lat, lon, div, near, radius){
     }
 
     function callback(results, status) {
-        
+
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 createMarker(results[i]);
@@ -170,7 +170,7 @@ function getGoogleMap(i,lat, lon, div, near, radius){
 
 // Searches YouTube for a single video q and appends it to a div
 
-function getYouTubeVideo(q,i) {
+function getYouTubeVideo(q, i) {
 
     $.get(`https://www.googleapis.com/youtube/v3/search`,
         {
@@ -187,18 +187,18 @@ function getYouTubeVideo(q,i) {
 
             // If the total results are 0, appends a gif of TV static, otherwise it appends the video
 
-            if(data.pageInfo.totalResults === 0){ 
+            if (data.pageInfo.totalResults === 0) {
 
-              $("#video-title-" + i).text("Not Found"); 
-              $("<img>").attr("src","assets/images/static.gif")
-                        .appendTo("#video-" + i);
+                $("#video-title-" + i).text("Not Found");
+                $("<img>").attr("src", "assets/images/static.gif")
+                    .appendTo("#video-" + i);
 
             }
-            else{
+            else {
 
-              $("#video-title-" + i).text("Watch"); 
-              $("<iframe>").attr("src", "https://www.youtube.com/embed/" + data.items[0].id.videoId)
-                           .attr("frameborder", "0").appendTo("#video-" + i);
+                $("#video-title-" + i).text("Watch");
+                $("<iframe>").attr("src", "https://www.youtube.com/embed/" + data.items[0].id.videoId)
+                    .attr("frameborder", "0").appendTo("#video-" + i);
 
             }
         });
@@ -207,108 +207,113 @@ function getYouTubeVideo(q,i) {
 
 // Returns a div with the classes passed as a parameter
 
-function newDiv(addClass){
+function newDiv(addClass) {
 
-  return $("<div>").addClass(addClass);
+    return $("<div>").addClass(addClass);
 
 }
 
 // Returns a span with the classes passed as a parameter
 
-function newSpan(addClass){
+function newSpan(addClass) {
 
-  return $("<span>").addClass(addClass);
+    return $("<span>").addClass(addClass);
 
 }
 
 // Creates a column based on the element name and the index i
 
-function createCol(element,i){
+function createCol(element, i) {
 
-  // Where the element showing data (videos maps) is rendered
+    // Where the element showing data (videos maps) is rendered
 
-  let dataGoesHere  = newDiv("embed-responsive embed-responsive-16by9")
-                                    .attr("id", element + "-" + i);
+    let dataGoesHere = newDiv("embed-responsive embed-responsive-16by9")
+        .attr("id", element + "-" + i);
 
-  // Where the title of the element is rendered
- 
-  let cardTitle     = newSpan("card-title")
-                                    .attr("id", element + "-title-" + i)
-                                    .text("Loading...");
+    // Where the title of the element is rendered
 
-  /* Below are CSS styles from the BandCram template */
+    let cardTitle = newSpan("card-title")
+        .attr("id", element + "-title-" + i)
+        .text("Loading...");
 
-  let cardContent   = newDiv("card-content")
-                                   .append(cardTitle);
- 
-  let cardImage      = newDiv("card-image")
-                                   .append(dataGoesHere)
-                                   .append(cardContent);
+    /* Below are CSS styles from the BandCram template */
 
-  let card          = newDiv("card")
-                                   .append(cardImage);
+    let cardContent = newDiv("card-content")
+        .append(cardTitle);
 
-  /* Above are CSS styles from the BandCram template */
+    let cardImage = newDiv("card-image")
+        .append(dataGoesHere)
+        .append(cardContent);
 
+    let card = newDiv("card")
+        .append(cardImage);
 
-  // Adds the card to a column
-  let col           = newDiv("col-xs-4")
-                                   .append(card);
-
-  // Returns all the nested elements together
-  return col;
-
-}
-
-function renderResult(i,title,desc){
-
-  // The panel for each element in the search list
-  let panel = newDiv("panel-body");
-
-  // The title of the panel
-  let panelTitle = $("<h1>").text(title);
-
-  // The subtitle of the panel
-  let panelDesc = $("<p>").text(desc);
-
-  panel.append(panelTitle);
-  panel.append(panelDesc);
-
-  // The row where the content is going to be added 
-  let row = newDiv("row");
+    /* Above are CSS styles from the BandCram template */
 
 
-  // Placeholder
-  let phCol = createCol("ph", i);
+    // Adds the card to a column
+    let col = newDiv("col-xs-4")
+        .append(card);
 
-  row.append(phCol);
-
-  // Map
-  let mapCol = createCol("map", i);
-
-  row.append(mapCol);
-
-  // Video
-  let videoCol = createCol("video", i);
-
-  row.append(videoCol);
-
-  panel.append(row);
-
-  let topPanel = newDiv("panel panel-default");
-
-  topPanel.append(panel);
-
-  // Appends the new panel to the results div
-  $("#results").append(topPanel);
-
+    // Returns all the nested elements together
+    return col;
 
 }
 
-$(document).ready(function(){
+function renderResult(i, title, desc) {
 
-  //getEvents("comedy","St Louis","February",10,"parking");
+    // The panel for each element in the search list
+    let panel = newDiv("panel-body");
 
-  getEventById("E0-001-106661781-3",0);
+    // The title of the panel
+    let panelTitle = $("<h1>").text(title);
+
+    // The subtitle of the panel
+    let panelDesc = $("<p>").text(desc);
+
+    panel.append(panelTitle);
+    panel.append(panelDesc);
+
+    // The row where the content is going to be added 
+    let row = newDiv("row");
+
+
+    // Placeholder
+    let phCol = createCol("ph", i);
+
+    row.append(phCol);
+
+    // Map
+    let mapCol = createCol("map", i);
+
+    row.append(mapCol);
+
+    // Video
+    let videoCol = createCol("video", i);
+
+    row.append(videoCol);
+
+    panel.append(row);
+
+    let topPanel = newDiv("panel panel-default");
+
+    topPanel.append(panel);
+
+    // Appends the new panel to the results div
+    $("#results").append(topPanel);
+
+
+}
+
+function timeFormat(eventTime) {
+    let eventTimeFormat = moment(eventTime).format("dddd, MMMM Do YYYY, h:mm a");
+    return eventTimeFormat;
+}
+$(document).ready(function () {
+
+    //getEvents("comedy","St Louis","February",10,"parking");
+
+    getEventById("E0-001-106661781-3", 0);
+    getEvents("concert", "Durham", "February", 10, "parking");
 
 });
